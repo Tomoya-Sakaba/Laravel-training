5af8ff12c722cfaf468b40e72dafa556f384f57f
Tomoya-Sakaba <kotementomo@gmail.com>
2023-07-10 12:20:35 +0900

    タブ機能追加の際のエラー

--- qa-laravel-ec-training/resources/views/user/tabs.blade.php
+++ qa-laravel-ec-training/resources/views/user/tabs.blade.php
@@ -2,18 +2,30 @@
 	
 @include('follow.follow_button', ['user' => $user])
 
-<ul class="nav nav-tabs nav-justified mt-5 mb-2">
-	<li class="nav-item">
-		<a class="nav-link active" aria-current="page" href="{{ route('mypage', ['id' => $user->id]) }}">
+<ul class="nav nav-tabs nav-justified mt-5 mb-2" role="tablist">
+	<li class="nav-item" role="presentation">
+		<a id="tab1" class="nav-link active" role="tab" data-bs-toggle="tab" aria-selected="true" href="{{ route('mypage', ['id' => $user->id]) }}">
 			投稿<br><div class="badge bg-secondary">{{ $count_posts }}</div>
 		</a>
 	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="{{ route('followers', ['id' => $user->id]) }}">
+	<li class="nav-item" role="presentation">
+		<a id="tab2" class="nav-link" role="tab" data-bs-toggle="tab" aria-selected="false" href="{{ route('followers', ['id' => $user->id]) }}">
 			フォロワー<br><div class="badge bg-secondary">{{ $count_followers }}</div></a>
 	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="{{ route('followings', ['id' => $user->id]) }}">
+	<li class="nav-item" role="presentation">
+		<a id="tab3" class="nav-link" role="tab" data-bs-toggle="tab" aria-selected="false" href="{{ route('followings', ['id' => $user->id]) }}">
 			フォロー中<br><div class="badge bg-secondary">{{ $count_followings }}</div></a>
 	</li>
 </ul>
+
+<div class="tab-content">
+	<div id="mypage/" class="tab-pane active" role="tabpanel" aria-labelledby="tab1">
+		@include('index', ['posts' => $posts])
+	</div>
+	<div id="user/*/followers" class="tab-pane" role="tabpanel" aria-labelledby="tab2">
+		@include('user.follow_index', ['posts' => $posts])
+	</div>
+	<div id="user/*/followings" class="tab-pane" role="tabpanel" aria-labelledby="tab3">
+		@include('user.follow_index', ['posts' => $posts])
+	</div>
+</div>
